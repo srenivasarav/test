8967c3c1b2c723d33b7a7852bd8eb8a65cdb1fce
Srinivasa Rao B <srenivas35@gmail.com>
2020-01-30 23:44:09 +0530

    AWS Introduction

---
+++ Data.sql
@@ -0,0 +1,8 @@
+Amazon Web Services(AWS) is a Amazon.com subsidiary which offers cloud-computing services at very affordable rates, therefore making its customer base strong from small scale companies like Pinterest (which has just 5 employees) to big enterprises like D-Link.
+
+SaaS (Software as a Service)
+It allows companies to use software without having to purchase them, which reduces the expenditure of the company drastically, since they are already installed on the cloud server they can be quickly deployed and therefore saves time.
+PaaS (Platform as a Service)
+It allows developers to build applications, collaborate on projects without having to purchase or maintain infrastructure.
+IaaS (Infrastructure as a Service)
+It allows companies to rent servers, storage space, etc. from a cloud provider. 
